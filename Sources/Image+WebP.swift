e725d27df6136daf1177666aaf1aa6771f12db15
Yang Chao <iyeatse@gmail.com>
2021-02-18 16:29:36 +0800

    fix: compile for macos

--- Sources/Image+WebP.swift
+++ Sources/Image+WebP.swift
@@ -19,24 +19,34 @@ extension KingfisherWrapper where Base: KFCrossPlatformImage {
     /// isLossy  (0=lossy , 1=lossless (default)).
     /// Note that the default values are isLossy= false and quality=75.0f
     public func webpRepresentation(isLossy: Bool = false, quality: Float = 75.0) -> Data? {
-        if let result = animatedWebPRepresentation() {
+        if let result = animatedWebPRepresentation(isLossy: isLossy, quality: quality) {
             return result
         }
+        #if os(macOS)
+        if let cgImage = base.cgImage(forProposedRect: nil, context: nil, hints: nil) {
+            return WebPDataCreateWithImage(cgImage, isLossy, quality) as Data?
+        }
+        #else
         if let cgImage = base.cgImage {
             return WebPDataCreateWithImage(cgImage, isLossy, quality) as Data?
         }
+        #endif
         return nil
     }
 
     /// isLossy  (0=lossy , 1=lossless (default)).
     /// Note that the default values are isLossy= false and quality=75.0f
     private func animatedWebPRepresentation(isLossy: Bool = false, quality: Float = 75.0) -> Data? {
+        #if os(macOS)
+        return nil
+        #else
         guard let images = base.images?.compactMap({ $0.cgImage }) else {
             return nil
         }
         let imageInfo = [ kWebPAnimatedImageFrames: images,
                           kWebPAnimatedImageDuration: NSNumber(value: base.duration) ] as [CFString : Any]
         return WebPDataCreateWithAnimatedImageInfo(imageInfo as CFDictionary, isLossy, quality) as Data?
+        #endif
     }
 }
 
@@ -47,7 +57,13 @@ extension KingfisherWrapper where Base: KFCrossPlatformImage {
         if (frameCount == 0) {
             return nil
         }
-
+        
+        #if os(macOS)
+        guard let cgImage = WebPImageCreateWithData(webpData as CFData) else {
+            return nil
+        }
+        return KFCrossPlatformImage(cgImage: cgImage, size: .zero)
+        #else
         if (frameCount == 1 || onlyFirstFrame) {
             guard let cgImage = WebPImageCreateWithData(webpData as CFData) else {
                 return nil
@@ -63,9 +79,9 @@ extension KingfisherWrapper where Base: KFCrossPlatformImage {
             return nil
         }
         let uiFrames = cgFrames.map { KFCrossPlatformImage(cgImage: $0, scale: scale, orientation: .up) }
-
         let duration = (animationInfo[kWebPAnimatedImageDuration] as? NSNumber).flatMap { $0.doubleValue as TimeInterval } ?? 0.1 * TimeInterval(frameCount)
         return KFCrossPlatformImage.animatedImage(with: uiFrames, duration: duration)
+        #endif
     }
 }
 
